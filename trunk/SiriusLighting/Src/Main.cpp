8e7242471dba036402f07f821e732371699aeb10
teexit4869@gmail.com <teexit4869@gmail.com@93767d43-1f6c-25c3-baac-9bfe1067fb63>
2012-02-20 13:56:23 +0000

    First arrange.

---
+++ trunk/SiriusLighting/Src/Main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+{
+	UNREFERENCED_PARAMETER( hPrevInstance );
+	UNREFERENCED_PARAMETER( lpCmdLine );
+
+	return 0;
+}
